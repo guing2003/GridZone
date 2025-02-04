c7b4fa97c319723c536a8749737bd0f911ffbe17
guing2003 <guilhermedelecrode1710@gmail.com>
2025-02-04 10:16:55 -0300

    Adicionando ICs e os Fragments das informações de cada seção

---
+++ app/src/main/java/com/guilhermedelecrode/gridzone/ui/CalendarioFragment.kt
@@ -0,0 +1,17 @@
+package com.guilhermedelecrode.gridzone.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.guilhermedelecrode.gridzone.R
+
+class CalendarioFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_calendario, container, false)
+    }
+}
