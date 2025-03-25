dd2fe5bf640e64ae6259493e03ab0b3bb0f21810
guing2003 <guilhermedelecrode1710@gmail.com>
2025-03-25 10:07:36 -0300

    Adiciona Adapter para PilotosCampeonato

---
+++ app/src/main/java/com/guilhermedelecrode/gridzone/adapter/PilotoCampeonatoFragmentAdapter.kt
@@ -0,0 +1,40 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.guilhermedelecrode.gridzone.R
+import com.guilhermedelecrode.gridzone.model.Piloto
+
+class PilotoCampeonatoFragmentAdapter(private val listaPilotos: List<Piloto>) :
+    RecyclerView.Adapter<PilotoCampeonatoFragmentAdapter.PilotoCampeonatoViewHolder>() {
+
+
+    class PilotoCampeonatoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //val imgPiloto: ImageView = itemView.findViewById(R.id.imagem_piloto_fragment)
+        val nomePiloto: TextView = itemView.findViewById(R.id.nome_piloto_campeonato_fragment)
+        val equipePiloto: TextView = itemView.findViewById(R.id.equipe_piloto_campeonato_fragment)
+        val pontuacaoPiloto: TextView = itemView.findViewById(R.id.pontuacao_piloto_campeonato_fragment)
+
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PilotoCampeonatoViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.piloto_campeonato_layout, parent, false)
+        return PilotoCampeonatoViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: PilotoCampeonatoViewHolder, position: Int) {
+        val piloto = listaPilotos[position]
+        //holder.imgPiloto.text =  // Define a imagem
+        holder.nomePiloto.text = piloto.nome
+        //holder.pontuacaoPiloto.text = piloto.pontuacao.toString()
+        holder.equipePiloto.text = piloto.equipe
+    }
+
+
+    override fun getItemCount() = listaPilotos.size
+}
--- app/src/main/java/com/guilhermedelecrode/gridzone/ui/Pilotos_Campeonato_Fragment.kt
+++ app/src/main/java/com/guilhermedelecrode/gridzone/ui/Pilotos_Campeonato_Fragment.kt
@@ -1,11 +1,15 @@
 package com.guilhermedelecrode.gridzone.ui
 
+import PilotoCampeonatoFragmentAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.guilhermedelecrode.gridzone.R
+import com.guilhermedelecrode.gridzone.model.Piloto
 
 class Pilotos_Campeonato_Fragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +17,20 @@ class Pilotos_Campeonato_Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_pilotos_campeonato, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView_campeonato_piloto_fragment)
+
+        val listaPilotos = listOf(
+            Piloto("Lewis Hamilton", "HAM", "44","Ferrari"),
+            Piloto("Charles Leclerc", "LEC", "16","Ferrari")
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = PilotoCampeonatoFragmentAdapter(listaPilotos)
     }
 
 }
