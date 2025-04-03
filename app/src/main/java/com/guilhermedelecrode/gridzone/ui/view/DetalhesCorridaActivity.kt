68c794ae6935160d597c9102ee8af757bbe57daf
guing2003 <guilhermedelecrode1710@gmail.com>
2025-04-03 10:10:12 -0300

    Adiciona Layout de DetalhesCorridaActivity

---
+++ app/src/main/java/com/guilhermedelecrode/gridzone/ui/view/DetalhesCorridaActivity.kt
@@ -0,0 +1,16 @@
+package com.guilhermedelecrode.gridzone.ui.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.guilhermedelecrode.gridzone.R
+
+class DetalhesCorridaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalhes_corrida)
+    }
+}
